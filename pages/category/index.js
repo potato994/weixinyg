2c1d27ccba7e1b14acabf46dd8cc782c5c26e7e7
potato <965693506@qq.com>
2019-07-29 19:28:23 +0800

    分类页面初次渲染完成

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,32 @@
-// ./pages/category/index.js
+import {request} from '../../request/index'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    LeftMenuList:[],
+    RightGoodsList:[],
+    currentIndex:0
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad(){
+    this.getAllLeftList()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  getAllLeftList(){
+    request({
+      url:"/categories"
+    }).then(res=>{
+      console.log(res);
+      let LeftMenuList = res.map((v,i)=>({ cat_name:v.cat_name,cat_id:v.cat_id }))
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+      let RightGoodsList = res[0].children
+      this.setData({
+        LeftMenuList,
+        RightGoodsList
+      })
+    })
   }
+ 
 })
