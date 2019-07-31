729b4afbc4d865a580e9dda6b2b05ec15d27771a
potato <965693506@qq.com>
2019-07-31 22:26:59 +0800

    商品列表（引入Tabs组件）

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -5,62 +5,21 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tabs: [
+      { id: 0, title: "综合", isActive: true },
+      { id: 1, title: "销量", isActive: false },
+      { id: 2, title: "价格", isActive: false },
+    ]
+  },
+  // 页面开始加载时，可以获取页面中的URL参数
+  onLoad(options) {
+    console.log(options);
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleTitleChange(e) {
+    const { index } = e.detail;
+    let {tabs} = this.data
+    tabs.forEach((v,i)=>i===index?v.isActive=true:v.isActive=false)
+    this.setData({tabs})
   }
 })
