e1ef1e4d4e54cbebc81c2902158fc42777087076
potato <965693506@qq.com>
2019-08-03 22:51:42 +0800

    支付页面-动态渲染

--- pages/pay/index.js
+++ pages/pay/index.js
@@ -1,66 +1,50 @@
-// ./pages/pay/index.js
+// ./pages/cart/index.js
+import regeneratorRuntime, { async } from '../../lib/runtime/runtime';
+
+import { getSetting, chooseAddress, openSetting, showModal } from "../../utils/asyncWx.js"
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    address: {},
+    cart: {},
+    totalNum: 0,
+    totalPrice: 0,
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  // 页面切换显示的时候触发 onShow
+  onShow() {
+    const address = wx.getStorageSync("address") || {};
+    // 获取购物车数据
+    const cart = wx.getStorageSync("cart") || {};
+
+    // 把address存入到data中
+    this.setData({ address })
+    this.setCart(cart)
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+  // 设置购物车数据和计算总价格
+  setCart(cart) {
+    // 把购物车对象 转成数据
+    let cartArr = Object.values(cart)
+    // 计算是否都选中了  
+    // every 会接收一个回调函数，当没有循环项都返回true的时候，cartArr.every的返回值才会是 true
+    let isAllChecked = cartArr.length ? cartArr.every(v => v.checked) : false;
+    // 计算总价格   只计算勾选了的价格
+    let totalPrice = 0;
+    let totalNum = 0;
+    cartArr.forEach(v => {
+      if (v.checked) {
+        totalPrice += v.num * v.goods_price;
+        totalNum += v.num
+      }
+    })
+    // 购物车是否有数据
+    let hascart = cartArr.length > 0 ? true : false;
+    this.setData({ cart, totalPrice, totalNum, isAllChecked,hascart })
+    // 防止数据改变了 刷新之后没有效果 所有也顺便存入缓存中
+    wx.setStorageSync("cart", cart);
 
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  
 })
